ab0a487d3a1f973250201066efadb0005370840b
alex <alex@alexs-mac-pro.radiobremen.de>
2019-06-05 16:01:58 +0200

    b

--- ascr/ViewController.swift
+++ ascr/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import CoreLocation
+import ARCL
 
 class ViewController: UIViewController {
 
+    var sceneLocationView = SceneLocationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sceneLocationView.run()
+        self.view.addSubview(sceneLocationView)
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 51.504571, longitude: -0.019717)
+        let location = CLLocation(coordinate: coordinate, altitude: 10)
+        let image = UIImage(named: "pin")!
+        let annotationNode = LocationAnnotationNode(location: location, image: image)
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sceneLocationView.frame = view.bounds
     }
 
 
